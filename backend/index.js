b29b5ae2af38789cbf16d128055d35184a83fff6
rahulbhati9965 <rahulbhati1127@gmail.com>
2026-02-11 17:22:50 +0530

    Fix: Express5 SPA fallback route

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,9 @@ app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running...");
